e63992d92d155aaa1bd03e5b49de5defcaa17d41
El alj Mouad <mouadelalj@gmail.com>
2024-02-27 17:24:53 +0100

    used language_with_cuda fixture to mark the tests that works with cuda

--- tests/conftest.py
+++ tests/conftest.py
@@ -15,11 +15,21 @@ if github_debugging:
         pytest.param("fortran", marks = pytest.mark.fortran),
         pytest.param("c", marks = pytest.mark.c),
         pytest.param("python", marks = pytest.mark.python),
-        pytest.param("cuda", marks = pytest.mark.cuda)
     ],
     scope = "session"
 )
 def language(request):
+    return request.param
+
+@pytest.fixture( params=[
+        pytest.param("fortran", marks = pytest.mark.fortran),
+        pytest.param("c", marks = pytest.mark.c),
+        pytest.param("python", marks = pytest.mark.python),
+        pytest.param("cuda", marks = pytest.mark.cuda)
+    ],
+    scope = "session"
+)
+def language_with_cuda(request):
     return request.param
 
 def move_coverage(path_dir):
