0013b65eb60992248d06142eeeedb989cffefd4a
said mazouz <smazouz@student.1337.ma>
2024-03-08 10:53:49 +0000

    Fix Codacy warning: Missing module docstring

--- pyccel/ast/cudaext.py
+++ pyccel/ast/cudaext.py
@@ -1,8 +1,18 @@
-
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#------------------------------------------------------------------------------------------#
+# This file is part of Pyccel which is released under MIT License. See the LICENSE file or #
+# go to https://github.com/pyccel/pyccel/blob/master/LICENSE for full license details.     #
+#------------------------------------------------------------------------------------------#
+"""
+CUDA Extension Module
+Provides CUDA functionality for code generation
+"""
 from .internals      import PyccelInternalFunction
 
 from .datatypes import (NativeVoid)
 from .core           import Module, PyccelFunctionDef
+
 __all__ = (
     'CudaSynchronize',
 )
