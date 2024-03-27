5c0aef70f82c438d944d6f5b348a88976081037f
said mazouz <smazouz@student.1337.ma>
2024-03-27 14:22:24 +0000

    debugging purpose

--- tests/pyccel/test_pyccel.py
+++ tests/pyccel/test_pyccel.py
@@ -362,7 +362,7 @@ def pyccel_test(test_file, dependencies = None, compile_with_pyccel = True,
                 The folder in which the generated files should be
                 saved
     """
-
+    
     rel_test_dir = os.path.dirname(test_file)
 
     test_file = os.path.normpath(test_file)
@@ -404,7 +404,7 @@ def pyccel_test(test_file, dependencies = None, compile_with_pyccel = True,
                 elif language == 'c':
                     compile_c(cwd, dependencies[i], [], is_mod = True)
                 elif language == 'cuda':
-                    compile_c(cwd, dependencies[i], [], is_mod = True)
+                    compile_cuda(cwd, dependencies[i], [], is_mod = True)
                 
             else:
                 # print(3)
@@ -430,7 +430,7 @@ def pyccel_test(test_file, dependencies = None, compile_with_pyccel = True,
         elif language == 'c':
             compile_c(cwd, output_test_file, dependencies)
         elif language == 'cuda':
-            compile_c(cwd, output_test_file, dependencies)
+            compile_cuda(cwd, output_test_file, dependencies)
 
     lang_out = get_lang_output(output_test_file, "c")
     compare_pyth_fort_output(pyth_out, lang_out, output_dtype, "c")
