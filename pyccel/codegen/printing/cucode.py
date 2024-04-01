8a127fb37af223eaf27c67f1b2e2e917fba6e7b1
said mazouz <smazouz@student.1337.ma>
2024-04-01 15:30:01 +0000

    Removed 'extern C' block from imports concatenation

--- pyccel/codegen/printing/cucode.py
+++ pyccel/codegen/printing/cucode.py
@@ -71,9 +71,8 @@ class CudaCodePrinter(CCodePrinter):
                 local_imports += self._print(imp)
 
         imports = f'{c_headers_imports}\
-                    extern "C"{{\n\
-                    {local_imports}\
-                    }}'
+                    {local_imports}'
+
 
         code = f'{imports}\n\
                  {global_variables}\n\
