ad0dbe989c35992671599f3eec397019d044608b
said mazouz <smazouz@student.1337.ma>
2024-04-02 13:51:19 +0000

    documented cuda synchronize

--- pyccel/cuda.py
+++ pyccel/cuda.py
@@ -6,9 +6,8 @@
 This module contains CUDA methods for Pyccel.
 """
 def synchronize():
-    """Synchronize CUDA device execution."""
+    """
+    Synchronize CUDA device execution.
 
-    """Synchronize CUDA device execution."""
-
-    # No action needed for synchronization at this point
-
+    Synchronize CUDA device execution.
+    """
