bafb7662f9e587aab4e43daa3db286da69a4c570
smazouz42 <smazouz@student.1337.ma>
2024-04-29 14:56:51 +0100

    adding both _wrap_FunctionDef && _wrap_Module

--- pyccel/codegen/wrapper/cuda_to_c_wrapper.py
+++ pyccel/codegen/wrapper/cuda_to_c_wrapper.py
@@ -4,16 +4,20 @@
 # go to https://github.com/pyccel/pyccel/blob/master/LICENSE for full license details.     #
 #------------------------------------------------------------------------------------------#
 """
-Module describing the code-wrapping class : CudaToCWrapper
+Module describing the code-wrapping class : CudaToPythonWrapper
 which creates an interface exposing Cuda code to C.
 """
 
+from pyccel.codegen.wrapper.c_to_python_wrapper import CToPythonWrapper
 from pyccel.parser.scope import Scope
-from pyccel.ast.core import Import, FunctionDef
-from pyccel.ast.cwrapper      import PyModule
-from pyccel.ast.core import AsName
+from pyccel.ast.core import Module
 from .wrapper import Wrapper
-
+from pyccel.ast.bind_c import BindCModule
+from pyccel.ast.core import Import
+from pyccel.ast.core import Module
+from pyccel.parser.scope import Scope
+from .wrapper import Wrapper
+cwrapper_ndarray_imports = [Import('cwrapper_ndarrays', Module('cwrapper_ndarrays', (), ()))]
 
 class CudaToCWrapper(Wrapper):
     """
@@ -24,73 +28,36 @@ class CudaToCWrapper(Wrapper):
     printed as an intermediary layer.
     """
     def __init__(self):
+        self._wrapper_names_dict = {}
         super().__init__()
-    
+
+
     def _wrap_Module(self, expr):
         """
-        Build a `PyModule` from a `Module`.
-        Create a `PyModule` which wraps a C-compatible `Module`.
+        Create a Module which is compatible with C.
+
+        Create a Module which provides an interface between C and the
+        Module described by expr
+
         Parameters
         ----------
-        expr : Module
-            The module which can be called from C.
+        expr : pyccel.ast.core.Module
+            The module to be wrapped.
+
         Returns
         -------
-        PyModule
-            The module which can be called from Python.
+        pyccel.ast.core.Module
+            The C-compatible module.
         """
         # Define scope
         scope = expr.scope
         mod_scope = Scope(used_symbols = scope.local_used_symbols.copy(), original_symbols = scope.python_names.copy())
         self.scope = mod_scope
 
-        imports = [self._wrap(i) for i in getattr(expr, 'original_module', expr).imports]
-        imports = [i for i in imports if i]
-
-        # Wrap classes
-        classes = [self._wrap(i) for i in expr.classes]
-
-        # Wrap functions
-        funcs_to_wrap = [f for f in expr.funcs if f not in (expr.init_func, expr.free_func)]
-        funcs_to_wrap = [f for f in funcs_to_wrap if not f.is_inline]
-
-        # Add any functions removed by the Fortran printer
-        removed_functions = getattr(expr, 'removed_functions', None)
-        if removed_functions:
-            funcs_to_wrap.extend(removed_functions)
-
-        funcs = [self._wrap(f) for f in funcs_to_wrap]
-
-        # Wrap interfaces
-        interfaces = [self._wrap(i) for i in expr.interfaces if not i.is_inline]
-
-        init_func = self._build_module_init_function(expr, imports)
-
-        API_var, import_func = self._build_module_import_function(expr)
-
+        name = mod_scope.get_new_name(f'bind_c_{expr.name.target}')
         self.exit_scope()
-        imports += cwrapper_ndarray_imports if self._wrapping_arrays else []
-
-        original_mod = getattr(expr, 'original_module', expr)
-
-        external_funcs = []
-        # Add external functions for normal functions
-        for f in expr.funcs:
-            external_funcs.append(FunctionDef(f.name.lower(), f.arguments, f.results, [], is_header = True, scope = Scope()))
-
-        for c in expr.classes:
-            m = c.new_func
-            external_funcs.append(FunctionDef(m.name, m.arguments, m.results, [], is_header = True, scope = Scope()))
-            for m in c.methods:
-                external_funcs.append(FunctionDef(m.name, m.arguments, m.results, [], is_header = True, scope = Scope()))
-            for i in c.interfaces:
-                for f in i.functions:
-                    external_funcs.append(FunctionDef(f.name, f.arguments, f.results, [], is_header = True, scope = Scope()))
-            for a in c.attributes:
-                for f in (a.getter, a.setter):
-                    external_funcs.append(FunctionDef(f.name, f.arguments, f.results, [], is_header = True, scope = Scope()))
-        return PyModule(original_mod.name, [API_var], funcs, imports = imports,
-                        interfaces = interfaces, classes = classes, scope = mod_scope,
-                        init_func = init_func, import_func = import_func , external_funcs = external_funcs)
-
-    
+        return BindCModule(name, expr.variables, expr.funcs,
+                scope = mod_scope,
+                original_module=expr)
+    def _wrap_FunctionDef(self, expr):
+        return expr
