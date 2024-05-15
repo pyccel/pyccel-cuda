10b0203849056b29024ab0f5404f345f63c9baca
smazouz42 <smazouz@student.1337.ma>
2024-05-15 10:19:17 +0100

    cleaning upmy PR

--- pyccel/codegen/printing/cucode.py
+++ pyccel/codegen/printing/cucode.py
@@ -11,7 +11,7 @@ enabling the direct translation of high-level Pyccel expressions into CUDA code.
 
 from pyccel.codegen.printing.ccode import CCodePrinter, c_library_headers
 
-from pyccel.ast.core        import Import, Module, AsName
+from pyccel.ast.core        import Import, Module
 
 from pyccel.errors.errors   import Errors
 
