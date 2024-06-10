4307d679d1682b0e433f20c84b42e0f3e612c2d7
smazouz42 <smazouz@student.1337.ma>
2024-06-10 16:23:57 +0100

    renaming cuda submodule and cleaning up the code

--- pyccel/cuda/__init__.py
+++ pyccel/cuda/__init__.py
@@ -1,3 +1,10 @@
-from .cuda_submodule1 import synchronize
+#------------------------------------------------------------------------------------------#
+# This file is part of Pyccel which is released under MIT License. See the LICENSE file or #
+# go to https://github.com/pyccel/pyccel/blob/master/LICENSE for full license details.     #
+#------------------------------------------------------------------------------------------#
+"""
+    This module is for exposing the CudaSubmodule functions.
+"""
+from .CudaSyncPrimitives    import synchronize
 
 __all__ = ['synchronize']
