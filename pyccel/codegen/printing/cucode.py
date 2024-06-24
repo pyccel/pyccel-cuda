a24effa17bd58fe69c3a632a39054443776dbc75
smazouz42 <smazouz@student.1337.ma>
2024-06-24 10:05:36 +0100

    debuging perpose

--- pyccel/codegen/printing/cucode.py
+++ pyccel/codegen/printing/cucode.py
@@ -62,75 +62,75 @@ class CudaCodePrinter(CCodePrinter):
         self.exit_scope()
         return code
 
-    # def function_signature(self, expr, print_arg_names = True):
-    #     """
-    #     Get the Cuda representation of the function signature.
+    def function_signature(self, expr, print_arg_names = True):
+        """
+        Get the Cuda representation of the function signature.
 
-    #     Extract from the function definition `expr` all the
-    #     information (name, input, output) needed to create the
-    #     function signature and return a string describing the
-    #     function.
-    #     This is not a declaration as the signature does not end
-    #     with a semi-colon.
+        Extract from the function definition `expr` all the
+        information (name, input, output) needed to create the
+        function signature and return a string describing the
+        function.
+        This is not a declaration as the signature does not end
+        with a semi-colon.
 
-    #     Parameters
-    #     ----------
-    #     expr : FunctionDef
-    #         The function definition for which a signature is needed.
+        Parameters
+        ----------
+        expr : FunctionDef
+            The function definition for which a signature is needed.
 
-    #     print_arg_names : bool, default : True
-    #         Indicates whether argument names should be printed.
+        print_arg_names : bool, default : True
+            Indicates whether argument names should be printed.
 
-    #     Returns
-    #     -------
-    #     str
-    #         Signature of the function.
-    #     """
-    #     cuda_decorater = '__global__' if('kernel' in expr.decorators) else ''
-    #     c_function_signature = super().function_signature(expr, print_arg_names)
-    #     return f'{cuda_decorater} {c_function_signature}'
+        Returns
+        -------
+        str
+            Signature of the function.
+        """
+        cuda_decorater = '__global__' if('kernel' in expr.decorators) else ''
+        c_function_signature = super().function_signature(expr, print_arg_names)
+        return f'{cuda_decorater} {c_function_signature}'
 
-    # def _print_KernelCall(self, expr):
-    #     func = expr.funcdef
-    #     args = []
-    #     for a in expr.args:
-    #         arg_val = a.value or Nil()
-    #         args.append(arg_val)
+    def _print_KernelCall(self, expr):
+        func = expr.funcdef
+        args = []
+        for a in expr.args:
+            arg_val = a.value or Nil()
+            args.append(arg_val)
 
-    #     args = ', '.join(self._print(a) for a in args)
-    #     return f"{func.name}<<<{expr.num_blocks}, {expr.tp_block}>>>({args});\n"
+        args = ', '.join(self._print(a) for a in args)
+        return f"{func.name}<<<{expr.num_blocks}, {expr.tp_block}>>>({args});\n"
 
-    # def _print_CudaSynchronize(self, expr):
-    #     return 'cudaDeviceSynchronize();\n'
+    def _print_CudaSynchronize(self, expr):
+        return 'cudaDeviceSynchronize();\n'
 
-    # def _print_ModuleHeader(self, expr):
-    #     self.set_scope(expr.module.scope)
-    #     self._in_header = True
-    #     name = expr.module.name
+    def _print_ModuleHeader(self, expr):
+        self.set_scope(expr.module.scope)
+        self._in_header = True
+        name = expr.module.name
 
-    #     funcs = ""
-    #     cuda_headers = ""
-    #     for f in expr.module.funcs:
-    #         if not f.is_inline:
-    #             if 'kernel' in f.decorators:  # Checking for 'kernel' decorator
-    #                 cuda_headers += self.function_signature(f) + ';\n'
-    #             else:
-    #                 funcs += self.function_signature(f) + ';\n'
-    #     global_variables = ''.join('extern '+self._print(d) for d in expr.module.declarations if not d.variable.is_private)
-    #     # Print imports last to be sure that all additional_imports have been collected
-    #     imports = [*expr.module.imports, *self._additional_imports.values()]
-    #     imports = ''.join(self._print(i) for i in imports)
+        funcs = ""
+        cuda_headers = ""
+        for f in expr.module.funcs:
+            if not f.is_inline:
+                if 'kernel' in f.decorators:  # Checking for 'kernel' decorator
+                    cuda_headers += self.function_signature(f) + ';\n'
+                else:
+                    funcs += self.function_signature(f) + ';\n'
+        global_variables = ''.join('extern '+self._print(d) for d in expr.module.declarations if not d.variable.is_private)
+        # Print imports last to be sure that all additional_imports have been collected
+        imports = [*expr.module.imports, *self._additional_imports.values()]
+        imports = ''.join(self._print(i) for i in imports)
 
-    #     self._in_header = False
-    #     self.exit_scope()
-    #     function_declaration = f'{cuda_headers}\n\
-    #                 extern "C"{{\n\
-    #                 {funcs}\
-    #                 }}\n'
-    #     return '\n'.join((f"#ifndef {name.upper()}_H",
-    #                       f"#define {name.upper()}_H",
-    #                       imports,
-    #                       global_variables,
-    #                       function_declaration,
-    #                       "#endif // {name.upper()}_H\n"))
+        self._in_header = False
+        self.exit_scope()
+        function_declaration = f'{cuda_headers}\n\
+                    extern "C"{{\n\
+                    {funcs}\
+                    }}\n'
+        return '\n'.join((f"#ifndef {name.upper()}_H",
+                          f"#define {name.upper()}_H",
+                          imports,
+                          global_variables,
+                          function_declaration,
+                          "#endif // {name.upper()}_H\n"))
 
